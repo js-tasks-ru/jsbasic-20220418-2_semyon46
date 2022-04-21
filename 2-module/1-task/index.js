e8a89fefd4d9ec47176c1531ec040617c16f79b1
Semyon <7on999@gmail.com>
2022-04-22 02:34:55 +0300

    made hw after 2 lesson

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
 function sumSalary(salaries) {
-  // ваш код...
+  const arrValues =  Object.values(salaries)
+  const filterArrValues =  arrValues.filter(a=>typeof(a)==='number' && !Number.isNaN(a) && a!==Infinity&&a!==-Infinity)
+  if (filterArrValues.length===0) {
+    return 0
+  } 
+  return filterArrValues.reduce((a,b)=>a+b)
 }
+
+const a = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  Bob: NaN,
+  Peter: Infinity,
+  Ivan: -Infinity,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+console.log(sumSalary(a))
+
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
+
 let calculator = {
-  // ваш код
+
+    read: function(a,b){
+      this.d=a
+      this.c=b
+     
+    },
+
+  sum(){return this.d+this.c},
+  mul(){return this.d*this.c},
 };
 
+
+
+console.log(calculator.returnVasya())
+calculator.read(2,5)
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
