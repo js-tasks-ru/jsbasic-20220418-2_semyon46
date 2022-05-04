b7fd7b6d2c80be68c4488f53ba39905cd6313d06
Семен Папин <papins@retair.ru>
2022-05-04 19:56:26 +0300

    сделал слайдер универсальным по отношению к кол-ву слайдов

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,44 +4,29 @@ function initCarousel() {
   const prevBtn = document.querySelector('.carousel__arrow_left')
   prevBtn.style.display = 'none'
   const slider = document.querySelector('.carousel__inner')
-
   let numberSlider = 1
 
+  if (slider.children.length===1){
+    nextBtn.style.display = 'none'
+    prevBtn.style.display = 'none'
+    return
+  }
+
   nextBtn.addEventListener('click', ()=>{
-    switch(numberSlider){
-      case 1:
-        slider.style.transform = `translateX(-${slider.offsetWidth}px)`
-        numberSlider++
-        prevBtn.style.display = ''
-        break
-      case 2:
-        slider.style.transform = `translateX(-${2*slider.offsetWidth}px)`
-        numberSlider++
-        break
-      case 3:
-        slider.style.transform = `translateX(-${3*slider.offsetWidth}px)`
-        numberSlider++
-        nextBtn.style.display = 'none'
-        break
+    slider.style.transform = `translateX(-${numberSlider*slider.offsetWidth}px)`
+    numberSlider++
+    prevBtn.style.display = ''
+    if (numberSlider===slider.children.length){
+      nextBtn.style.display = 'none'
     }
   })
 
   prevBtn.addEventListener('click', ()=>{
-    switch(numberSlider){
-      case 2:
-        slider.style.transform = `translateX(0px)`
-        numberSlider--
-        prevBtn.style.display = 'none'
-        break
-      case 3:
-        slider.style.transform = `translateX(-${1*slider.offsetWidth}px)`
-        numberSlider--
-        break
-      case 4:
-        slider.style.transform = `translateX(-${2*slider.offsetWidth}px)`
-        numberSlider--
-        nextBtn.style.display = ''
-        break
+    slider.style.transform = `translateX(-${(numberSlider-2)*slider.offsetWidth}px)`
+    numberSlider--
+    nextBtn.style.display = ''
+    if (numberSlider===1){
+      prevBtn.style.display = 'none'
     }
   })
 }
